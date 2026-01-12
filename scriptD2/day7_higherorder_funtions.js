f4f1058f9dd7c46c7a03c81ba35808412fe4db61
Deployment-SYS <ttestrsys@gmail.com>
2026-01-12 07:21:26 +0530

    changes

--- scriptD2/day7_higherorder_funtions.js
+++ scriptD2/day7_higherorder_funtions.js
@@ -8,39 +8,30 @@ Multiply
 //Higher-order funtion
 //a) assign func to variable
 //b) pass a function as parameter
-/*
+
 function calculate(a,b,fun){
     return fun(a,b)
 }
-
 let result = calculate(3,3,divide)
 console.log(result)
-
 //sum
 //let sum = (a,b)=> {return a+b}
-
 function sum(a,b){
     return a+b
 }
-
 //minus
-
 function minus(a,b){
     return a-b
 }
-
 //multiply
-
 function multiply(a,b){
     return a*b
 }
-
 //divide
-
 function divide(a,b){
     return a/b
 }
-*/
+
 
 //c) return a function from a function
 function check(){
@@ -50,3 +41,5 @@ function check(){
 let checkresult = check()
 console.log(check())
 console.log(checkresult(4,5))
+
+
